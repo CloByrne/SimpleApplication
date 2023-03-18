757d50364ebecf80eae09884ee2f9e3e74245e1d
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-03-18 18:03:15 +0000

    updated pipeline to be secured with https

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
-
-
 sudo apt update && sudo apt install nodejs npm
 
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
@@ -16,8 +14,8 @@ cd SimpleApplication/
 # Install application dependancies
 npm install
 
-# echo $PRIVATE_KEY > privatekey.pem
-# echo $SERVER > server.crt
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 
 # Start the application with the process name SimpleApplication using pm2
 pm2 start ./bin/www --name SimpleApplication
